5ee9cbbb79d0881e297640f14d6dac9621ebabe9
Markatov Sergiy <marivankatov@gmail.com>
2018-12-19 22:28:44 +0200

    Fixed some errors in sync2s3.sh

--- cron/scripts/sync2s3.sh
+++ cron/scripts/sync2s3.sh
@@ -6,11 +6,12 @@ set -a
 AWS_CONFIG_FOLDER="$(dirname ${AWS_CONFIG})"
 
 # Getting ID of redmine database container
-peer_cont_id=$(docker container ls --filter 'label=ru.sdaminfo.description=Redmine database service' -q | head -n1)
+peer_cont_id=$(docker container ls --filter 'label=com.bolyshev.redmine.description=Redmine database service' -q | head -n1)
 
 docker run --privileged --rm \
            --name sync_to_aws_s3 \
-           --volumes-from ${redmine_db_id} \
-           -v "${AWS_CONFIG_FOLDER}:${AWS_CONFIG_FOLDER}" \
+           --volumes-from ${peer_cont_id} \
+           -e "AWS_S3_BUCKET=${AWS_S3_BUCKET}" \
+           -v "${AWS_CONFIG_FOLDER}:/root/.aws" \
            ${AWS_S3_IMAGE}
 
